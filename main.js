38ed780ea3da3289c973c023f279882efb033c05
Hali Tran <78198010+HaliTran@users.noreply.github.com>
2023-11-11 21:41:48 -0800

    Change radio buttons to checkbox

--- main.js
+++ main.js
@@ -5,29 +5,32 @@ function addTaskBtnHandler(event) {
     const listOfTasks = document.querySelector('#list-of-tasks');
     const task = document.querySelector('#task-to-add');
     const newTaskContainer = document.createElement('article');
-    const radioInput = document.createElement('input');
+    const checkBoxInput = document.createElement('input');
     const taskDescription = document.createElement('p');
 
     if (task.value === '') return;
 
     const inputAttribute = {
-      type: 'radio',
-      id: `task-${taskId}`,
+      type: 'checkbox',
       name: `task-${taskId}`,
-      'aria-label': 'task',
+      value: task.value,
+      'aria-label': task.value,
     };
-    taskId += 1;
 
     for (const key in inputAttribute) {
-      radioInput.setAttribute(key, inputAttribute[key]);
+      checkBoxInput.setAttribute(key, inputAttribute[key]);
     }
     taskDescription.innerText = task.value;
 
-    newTaskContainer.append(radioInput);
+    newTaskContainer.classList.add(`task-${taskId}`);
+    newTaskContainer.append(checkBoxInput);
     newTaskContainer.append(taskDescription);
 
     listOfTasks.append(newTaskContainer);
     task.value = '';
+
+    // increment task id to unique identify another task later
+    taskId += 1;
   }
 }
 
