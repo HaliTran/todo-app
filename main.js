463aa4386980f9285d248d0fd06641d2e0fa228b
Hali Tran <78198010+HaliTran@users.noreply.github.com>
2023-11-11 21:23:02 -0800

    Add radio input attributes

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
+let taskId = 0;
+
 function addTaskBtnHandler(event) {
   if (event.type === 'click' || event.key === 'Enter') {
     const listOfTasks = document.querySelector('#list-of-tasks');
     const task = document.querySelector('#task-to-add');
-    const newTask = document.createElement('div');
-    newTask.innerHTML = `<p>${task.value}</p>`;
-    listOfTasks.append(newTask);
+    const newTaskContainer = document.createElement('article');
+    const radioInput = document.createElement('input');
+    const taskDescription = document.createElement('p');
+
+    if (task.value === '') return;
+
+    const inputAttribute = {
+      type: 'radio',
+      id: `task-${taskId}`,
+      name: `task-${taskId}`,
+      'aria-label': 'task',
+    };
+    taskId += 1;
+
+    for (const key in inputAttribute) {
+      radioInput.setAttribute(key, inputAttribute[key]);
+    }
+    taskDescription.innerText = task.value;
+
+    newTaskContainer.append(radioInput);
+    newTaskContainer.append(taskDescription);
+
+    listOfTasks.append(newTaskContainer);
     task.value = '';
   }
 }
